78582fca5eefee8e20dcdb5c8c9c3dccc97eb5fb
Phong <139188133+amenphong250803@users.noreply.github.com>
2026-03-22 14:55:15 +0700

    Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputActionReference moveAction;
+
+    public CharacterController charCon;
+
+    public float moveSpeed;
+
+    private float ySpeed;
+
+    public InputActionReference jumpAction;
+
+    public float jumpForce;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +24,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
+
+        Vector3 moveAmount = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        moveAmount = moveAmount * moveSpeed;
+
+        if (charCon.isGrounded == true)
+        {
+            ySpeed = 0f;
+
+            if (jumpAction.action.WasPressedThisFrame())
+            {
+                ySpeed = jumpForce;
+            }
+        }
+
+        ySpeed = ySpeed + (Physics.gravity.y * Time.deltaTime);
         
+
+        moveAmount.y = ySpeed;
+
+        charCon.Move(moveAmount * Time.deltaTime);
     }
 }
