b491c6e876a2d61c1f0962586d6a22415c44a49c
Phong <139188133+amenphong250803@users.noreply.github.com>
2026-03-23 06:38:37 +0700

    The player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,21 +12,41 @@ public class PlayerController : MonoBehaviour
     private float ySpeed;
 
     public InputActionReference jumpAction;
-
     public float jumpForce;
+
+    public InputActionReference lookAction;
+    private float horiRot, vertRot;
+    public float lookSpeed;
+    public Transform theCam;
+    public float minLookAngle, maxLookAngle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 lookinput = lookAction.action.ReadValue<Vector2>();
+
+        horiRot += lookinput.x * Time.deltaTime * lookSpeed;
+        transform.rotation = Quaternion.Euler(0f, horiRot, 0f);
+
+        vertRot -= lookinput.y * Time.deltaTime * lookSpeed;
+        vertRot = Mathf.Clamp(vertRot, minLookAngle, maxLookAngle);
+        theCam.localRotation = Quaternion.Euler(vertRot, 0f, 0f);
+
+
+
         Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
 
-        Vector3 moveAmount = new Vector3(moveInput.x, 0f, moveInput.y);
+        Vector3 vertMove = transform.forward * moveInput.y;
+        Vector3 horiMove = transform.right * moveInput.x;
+
+        Vector3 moveAmount = horiMove + vertMove;
+        moveAmount = moveAmount.normalized;
 
         moveAmount = moveAmount * moveSpeed;
 
