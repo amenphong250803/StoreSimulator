ca6e8d8358f42dcd0149724b3760cce71845927e
Phong <139188133+amenphong250803@users.noreply.github.com>
2026-03-30 23:03:13 +0700

    Boxes of Stock

--- Assets/Scripts/StockObject.cs
+++ Assets/Scripts/StockObject.cs
@@ -55,4 +55,10 @@ public class StockObject : MonoBehaviour
 
         col.enabled = true;
     }
+
+    public void PlaceInBox()
+    {
+        theRB.isKinematic = true;
+        col.enabled = false;
+    }
 }
